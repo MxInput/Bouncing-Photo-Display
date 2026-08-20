b47fa37dd098639281ad4ea05e5a787f09f9364b
MxInput <mxinput@protonmail.com>
2026-08-20 03:57:55 -0400

    adding effects

--- script.js
+++ script.js
@@ -1,22 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
+  var count = 0;
+
+  var counter = document.getElementById("counter");
+  var instructions = document.getElementById("instructions");
+
+  var star = document.getElementById("star");
+
   const elements = document.querySelectorAll('.bounce');
+  const color_lib = ['darkolivegreen', 'darkslategray', 'darkseagreen', 'darkorchid', 'darkturquoise'];
 
   const items = Array.from(elements).map((el, i) => {
     const item = {
       el: el,
       x: 50 + i * 40,
       y: 50 + i * 40,
-      dx: (Math.random() < 0.5 ? 1 : -1) * (0.75 + Math.random() * 1.5), /* adjust these values for speed!*/
+      dx: (Math.random() < 0.5 ? 1 : -1) * (0.75 + Math.random() * 1.5),
       dy: (Math.random() < 0.5 ? 1 : -1) * (0.75 + Math.random() * 1.5),
-      isHovered: false
+      isHovered: false,
+      hovered_on_before: false
     };
 
     el.addEventListener('mouseenter', () => {
       item.isHovered = true;
+      if (!item.hovered_on_before && item.el.nodeName == "IMG") {
+        item.hovered_on_before = true;
+
+        count++;
+
+        counter.innerHTML = "HOVER COUNT: " + count.toString();
+
+        if (count >= 6) {
+          instructions.innerHTML = "CONGRATS! YOU GOT THEM ALL!!! HAVE A GOLD STAR";  
+          star.style.visibility = "visible";
+        }
+      }
+
+      if (item.el.id == "reverse") {
+        item.el.style.transform = 'rotate(160deg)';
+      }
+      if (item.el.id == "small") {
+        item.el.style.maxWidth = '10vw';
+      }
+      if (item.el.id == "big") {
+        item.el.style.maxWidth = '30vw';
+      }
+      if (item.el.id == "border_color") {
+        item.el.style.borderColor = 'red';
+      }
+      if (item.el.id == "hide") {
+        item.el.style.opacity = 0.5;
+      }
     });
 
     el.addEventListener('mouseleave', () => {
       item.isHovered = false;
+
+      if (item.el.id == "reverse") {
+        item.el.style.transform = 'rotate(0deg)';
+      }    
+      if (item.el.id == "small" || item.el.id == "big") {
+        item.el.style.maxWidth = '15vw';
+      }      
+      if (item.el.id == "border_color") {
+        item.el.style.borderColor = 'white';
+      }
+      if (item.el.id == "hide") {
+        item.el.style.opacity = 1.0;
+      }
     });
 
     return item;
@@ -35,15 +85,37 @@ document.addEventListener('DOMContentLoaded', () => {
       if (item.x <= 0) {
         item.x = 0;
         item.dx = Math.abs(item.dx);
+
+        if (item.el.nodeName == "H1" || item.el.nodeName == "P") {
+          let i = Math.floor(Math.random() * color_lib.length);
+          let r = color_lib[i];
+          item.el.style.color = r;
+        }
+          
       } else if (item.x >= maxW) {
+         if (item.el.nodeName == "H1" || item.el.nodeName == "P") {
+          let i = Math.floor(Math.random() * color_lib.length);
+          let r = color_lib[i];
+          item.el.style.color = r;
+        }
         item.x = maxW;
         item.dx = -Math.abs(item.dx);
       }
 
       if (item.y <= 0) {
+        if (item.el.nodeName == "H1" || item.el.nodeName == "P") {
+          let i = Math.floor(Math.random() * color_lib.length);
+          let r = color_lib[i];
+          item.el.style.color = r;
+        }
         item.y = 0;
         item.dy = Math.abs(item.dy);
       } else if (item.y >= maxH) {
+        if (item.el.nodeName == "H1" || item.el.nodeName == "P") {
+          let i = Math.floor(Math.random() * color_lib.length);
+          let r = color_lib[i];
+          item.el.style.color = r;
+        }
         item.y = maxH;
         item.dy = -Math.abs(item.dy);
       }
